26f18c4aec72d901b45b6dc6c4bc961f10bc4faa
Yogendra <you@example.com>
2022-01-03 12:54:30 +0530

    ADDED : Create Multiple AddressBook And Store Data

--- src/main/java/com/example/demo/bridgelabz/controller/AddressBookController.java
+++ src/main/java/com/example/demo/bridgelabz/controller/AddressBookController.java
@@ -5,22 +5,17 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-
 import org.springframework.web.bind.annotation.PostMapping;
-
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.example.demo.bridgelabz.dto.AddressBookDTO;
-
 import com.example.demo.bridgelabz.dto.ResponseDTO;
 import com.example.demo.bridgelabz.model.AddressBookModel;
 
 import com.example.demo.bridgelabz.service.IAddressBookService;
 
 @RestController
-@RequestMapping("/addressbook")
 public class AddressBookController {
 
 	@Autowired
